fbe18680ed6454c8136d584cfe8ad1105a27342e
Anna Tatarynova <tatarynowanya2003@gmail.com>
2026-06-03 18:28:46 +0100

    Fix empty pages

---
+++ app/learn/workshops/page.tsx
@@ -0,0 +1,7 @@
+export default function WorkshopsPage() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-light text-primary-dark">WorkShops</h1>
+    </main>
+  );
+}
